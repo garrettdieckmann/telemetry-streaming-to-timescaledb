2825403368ccf49201d479f6db9050a3ab6feab1
Garrett Dieckmann <g.dieckmann@f5.com>
2021-11-10 15:01:37 -0800

    move config to yml file

--- explorations/timescaledb/pythonML/withTSdata.py
+++ explorations/timescaledb/pythonML/withTSdata.py
@@ -4,39 +4,22 @@ import psycopg2.extras
 import pandas as pd
 import numpy as np
 import datetime
+import yaml
 from pmdarima.arima import auto_arima
 
 timescale_host = os.environ.get('TS_HOST')
 timescale_user = os.environ.get('TS_USER')
 timescale_password = os.environ.get('TS_PASSWORD')
-timescale_port = os.environ.get('TS_PORT')
+timescale_port = os.getenv('TS_PORT', 5432)
 
 # establish connection
 conn = psycopg2.connect(host=timescale_host, user=timescale_user, password=timescale_password, port=timescale_port)
 
-# cursor object allows querying of database
-# server-side cursor is created to prevent records to be downloaded until explicitly fetched
-cursor = conn.cursor()
 
-# execute SQL query
-cursor.execute('''
-SELECT
-  time_bucket('60.000s',"time") AS "bucketed_time",
-  avg(value) AS "system_cpu",
-  jsonb(labels) ->> 'hostname' as "hostname"
-FROM system_cpu
-GROUP BY bucketed_time, hostname
-ORDER BY bucketed_time
-''')
-
-# make records into a pandas dataframe
-all_time_series_df = pd.DataFrame(np.array(cursor.fetchall()), columns = ['bucketed_time', 'system_cpu', 'hostname'])
-
-
-def make_model_by_hostname(df):
+def make_model_by_dimension(df, dimension_name):
     # set the index of dataframes to the 'bucketed_time'
     df.set_index('bucketed_time', inplace = True)
-    host_df = df.drop(['hostname'], axis = 1)
+    host_df = df.drop([dimension_name], axis = 1)
     host_df.dropna(inplace = True)
     # convert trip_length into a numeric value in seconds
     arima_model = auto_arima(host_df)
@@ -52,8 +35,8 @@ def make_model_by_hostname(df):
     with_conf_interval.columns = ['lower_bounds', 'upper_bounds']
     return with_conf_interval
 
-def replace_model_predictions(model, hostname):
-    model['hostname'] = hostname
+def replace_model_predictions(model, dimension_name, dimension):
+    model[dimension_name] = dimension
     model.reset_index(inplace=True)
     df_columns = list(model)
     # create (col1,col2,...)
@@ -63,19 +46,32 @@ def replace_model_predictions(model, hostname):
     values = "VALUES({})".format(",".join(["%s" for _ in df_columns])) 
 
     cur = conn.cursor()
-    cur.execute("DELETE FROM predictions_system_cpu WHERE hostname = %s", (hostname,))
+    cur.execute("DELETE FROM %s WHERE %s = %s", ('TABLE_NAME', 'DIMENSION', dimension))
     conn.commit()
 
     # create INSERT INTO table (columns) VALUES('%s',...)
-    insert_stmt = "INSERT INTO predictions_system_cpu ({}) {}".format(columns,values)
+    insert_stmt = "INSERT INTO %s ({}) {}".format('TABLE_NAME', columns, values)
 
     psycopg2.extras.execute_batch(cur, insert_stmt, model.values)
     conn.commit()
     cur.close()
 
-for hostname in all_time_series_df.hostname.unique():
-    df = all_time_series_df.loc[all_time_series_df['hostname'] == hostname]
-    model = make_model_by_hostname(df)
-    replace_model_predictions(model, hostname)
+def process_by_the_dimension(dimension_name, all_time_series_df):
+    for dimension in all_time_series_df[dimension_name].unique():
+        df = all_time_series_df.loc[all_time_series_df[dimension_name] == dimension]
+        model = make_model_by_dimension(df, dimension_name)
+        replace_model_predictions(model, dimension_name, dimension)
 
-cursor.close()
+with open("model_configuration.yaml", 'r') as stream:
+    loaded_configuration = yaml.safe_load(stream)
+
+for model_config in loaded_configuration['models']:
+    cursor = conn.cursor()
+    # execute SQL query
+    cursor.execute(model_config['sql_query'])
+    # make records into a pandas dataframe
+    columns = ['bucketed_time', 'value']
+    columns.extend([model_config['dimension']])
+    all_time_series_df = pd.DataFrame(np.array(cursor.fetchall()), columns = columns)
+    process_by_the_dimension(model_config['dimension'], all_time_series_df)
+    cursor.close()
